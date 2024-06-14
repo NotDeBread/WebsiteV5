270f2be262d58f5690dc609eeee6ef05cad72a78
DeBread <104747002+NotDeBread@users.noreply.github.com>
2024-06-14 01:16:56 -0500

    v0.01

---
+++ cursor.js
@@ -0,0 +1,336 @@
+const cursor = doge('cursor')
+const realCursor = doge('realCursor')
+const cursorText = doge('cursorText')
+let mouse = [0, 0]
+let cursorX = 0
+let cursorY = 0
+let cursorHovering = undefined
+let customContextMenu = true
+
+const speed = 0.15
+let mouseTarget = () => {return [mouse[0], mouse[1]]}
+
+function updateCursor() {
+    const dx = mouseTarget()[0] - cursorX
+    const dy = mouseTarget()[1] - cursorY
+    cursorX += dx * speed
+    cursorY += dy * speed
+    cursor.style.translate = `${cursorX - cursor.offsetWidth / 2}px ${cursorY - cursor.offsetHeight / 2}px`
+    realCursor.style.translate = `${mouse[0] - realCursor.offsetWidth / 2}px ${mouse[1] - realCursor.offsetHeight / 2}px`
+    cursorText.style.translate = `${cursorX - cursorText.offsetWidth / 2}px ${(cursorY - cursor.offsetHeight / 2) - cursorText.offsetHeight - 2}px`
+
+    requestAnimationFrame(updateCursor)
+}
+
+document.addEventListener('mousemove', (e) => {
+    mouse[0] = e.pageX
+    mouse[1] = e.pageY
+})
+updateCursor()
+
+document.addEventListener('mousedown', () => {
+    if(cursorHovering === undefined) {
+        cursor.style.scale = 0.75
+    }
+})
+document.addEventListener('mouseup', () => {cursor.style.scale = 1})
+
+document.querySelectorAll('span, em').forEach(text => {
+    if(!text.getAttribute('ignoreCursor')) {
+        text.onmouseenter = () => {
+            if(!contextMenuOpen) {
+                mouseTarget = () => {return [mouse[0], text.getBoundingClientRect().top + text.offsetHeight / 2]}
+                cursor.style.height = text.offsetHeight + 'px'
+                cursor.style.width = '5px'
+                cursor.style.borderRadius = '0'
+                cursorHovering = text
+            }
+        }
+        text.onmouseleave = () => {
+            if(!contextMenuOpen) {
+                mouseTarget = () => {return [mouse[0], mouse[1]]}
+                cursor.style.height = '15px'
+                cursor.style.width = '15px'
+                cursor.style.borderRadius = '50%'
+                cursorHovering = undefined
+            }
+        }
+    }
+})
+
+document.querySelectorAll('button').forEach(button => {
+    button.onmouseenter = () => {
+        if(!contextMenuOpen) {
+            const rect = button.getBoundingClientRect()
+            mouseTarget = () => {return [rect.left + button.offsetWidth / 2, rect.top + button.offsetHeight / 2]}
+            cursor.style.borderRadius = '0'
+            cursor.style.width = button.offsetWidth + 'px'
+            cursor.style.height = button.offsetHeight + 'px'
+            cursorHovering = button
+        }
+    }
+    button.onmouseleave = () => {
+        if(!contextMenuOpen) {
+            mouseTarget = () => {return [mouse[0], mouse[1]]}
+            cursor.style.borderRadius = '50%'
+            cursor.style.width = '15px'
+            cursor.style.height = '15px'
+            cursorHovering = undefined
+        }
+    }
+})
+
+document.querySelectorAll('img').forEach(img => {
+    if(!img.getAttribute('ignoreCursor')) {
+        img.onmouseenter = () => {
+            if(!contextMenuOpen && !img.getAttribute('ignore')) {
+                const rect = img.getBoundingClientRect()
+                mouseTarget = () => {return [rect.left + img.offsetWidth / 2, rect.top + img.offsetHeight / 2]}
+                cursor.style.borderRadius = '0'
+                cursor.style.width = img.offsetWidth + 'px'
+                cursor.style.height = img.offsetHeight + 'px'
+                cursor.style.outline = '2px solid white'
+                cursor.style.backdropFilter = 'none'
+                cursorHovering = img
+            }
+        }
+        img.onmouseleave = () => {
+            if(!contextMenuOpen && !img.getAttribute('ignore')) {
+                mouseTarget = () => {return [mouse[0], mouse[1]]}
+                cursor.style.borderRadius = '50%'
+                cursor.style.width = '15px'
+                cursor.style.height = '15px'
+                cursor.style.outline = 'none'
+                cursor.style.backdropFilter = 'invert()'
+                cursorHovering = undefined
+            }
+        }
+    }
+})
+
+document.querySelectorAll('.game').forEach(game => {
+    game.onmouseenter = () => {
+        if(!contextMenuOpen && !game.getAttribute('ignore')) {
+            const rect = game.getBoundingClientRect()
+            mouseTarget = () => {return [rect.left + game.offsetWidth / 2, rect.top + game.offsetHeight / 2]}
+            cursor.style.borderRadius = '0'
+            cursor.style.width = game.offsetWidth + 'px'
+            cursor.style.height = game.offsetHeight + 'px'
+            cursor.style.outline = '4px solid white'
+            cursor.style.backdropFilter = 'none'
+            cursorHovering = game
+        }
+    }
+    game.onmouseleave = () => {
+        if(!contextMenuOpen && !game.getAttribute('ignore')) {
+            mouseTarget = () => {return [mouse[0], mouse[1]]}
+            cursor.style.borderRadius = '50%'
+            cursor.style.width = '15px'
+            cursor.style.height = '15px'
+            cursor.style.outline = 'none'
+            cursor.style.backdropFilter = 'invert()'
+            cursorHovering = undefined
+        }
+    }
+    game.onclick = () => {
+        if(game.getAttribute('game')) {
+            window.open(`https://debread.space/${game.getAttribute('game')}`, '_blank')
+        }
+    }
+})
+
+document.querySelectorAll('a').forEach(link => {
+    link.target = '_blank'
+    link.onmouseenter = () => {
+        if(!contextMenuOpen) {
+            const rect = link.getBoundingClientRect()
+            mouseTarget = () => {return [rect.left + link.offsetWidth / 2, rect.top + link.offsetHeight / 2]}
+            cursor.style.borderRadius = '0'
+            cursor.style.width = link.offsetWidth + 'px'
+            cursor.style.height = link.offsetHeight + 'px'
+            cursor.style.outline = '2px solid white'
+            cursor.style.backdropFilter = 'none'
+            cursorHovering = link
+            cursorText.innerText = link.href.replace('https://','')
+        }
+    }
+    link.onmouseleave = () => {
+        if(!contextMenuOpen) {
+            mouseTarget = () => {return [mouse[0], mouse[1]]}
+            cursor.style.borderRadius = '50%'
+            cursor.style.width = '15px'
+            cursor.style.height = '15px'
+            cursor.style.outline = 'none'
+            cursor.style.backdropFilter = 'invert()'
+            cursorHovering = undefined
+            cursorText.innerText = ''
+        }
+    }
+})
+
+let contextMenuOpen = false
+document.addEventListener('contextmenu', ev => {
+    if(customContextMenu) {
+        ev.preventDefault()
+        cursor.innerHTML = ''
+        if(contextMenuOpen) {
+            closeContextMenu()
+        } else {
+            openContextMenu()
+        }
+    }
+})
+
+cursor.onmouseleave = () => {
+    closeContextMenu(true)
+}
+
+function openContextMenu() {
+    const buttons = []
+
+    const selectedText = window.getSelection().toString()
+    
+    if(cursorHovering) {
+        if(cursorHovering.src) {
+            buttons.push({
+                text: 'Dowload image',
+                action: () => {
+                    const link = document.createElement('a')
+                    link.href = cursorHovering.src
+                    link.download = cursorHovering.src.substring(cursorHovering.src.lastIndexOf('/') + 1)
+                    document.body.append(link)
+                    link.click()
+                    link.remove()
+                }
+            })
+            buttons.push({
+                text: 'Copy image url',
+                action: () => {
+                    navigator.clipboard.writeText(cursorHovering.src)
+                }
+            })
+            buttons.push({
+                text: 'Open image',
+                action: () => {
+                    window.open(cursorHovering.src)
+                }
+            })
+        }
+        if(cursorHovering.href) {
+            buttons.push({
+                text: 'Open in new tab',
+                action: () => {
+                    window.open(cursorHovering.href, '_blank')
+                }
+            })
+            buttons.push({
+                text: 'Open here',
+                action: () => {
+                    window.open(cursorHovering.href, '_self')
+                }
+            })
+            buttons.push({
+                text: 'Copy link',
+                action: () => {
+                    navigator.clipboard.writeText(cursorHovering.href)
+                }
+            })
+        }
+
+        if(cursorHovering.classList.contains('game')) {
+            buttons.push({
+                text: 'Open game in new tab',
+                action: () => {
+                    window.open(`https://debread.space/${cursorHovering.getAttribute('game')}`, '_blank')
+                }
+            })
+            buttons.push({
+                text: 'Open game here',
+                action: () => {
+                    window.open(`https://debread.space/${cursorHovering.getAttribute('game')}`, '_self')
+                }
+            })
+        }
+
+    }
+
+    if(window.getSelection().toString().length > 0) {
+        buttons.push({
+            text: 'Copy',
+            action: () => {
+                navigator.clipboard.writeText(selectedText)
+            }
+        })
+    }
+
+    buttons.push({
+        text: 'Disable context menu',
+        action: () => {
+            customContextMenu = false
+        }
+    })
+
+    buttons.push({
+        text: 'Copy page link',
+        action: () => {navigator.clipboard.writeText('https://debread.space')}
+    })
+
+    buttons.push({
+        text: 'Reload page',
+        action: () => {window.location.reload()}
+    })
+
+    if(buttons.length === 0) {
+        closeContextMenu(false)
+        contextMenuOpen = false
+    } else {
+        contextMenuOpen = true
+    }
+
+    if(contextMenuOpen) {
+        const currentMousePos = [mouse[0], mouse[1]]
+        mouseTarget = () => {return [currentMousePos[0], currentMousePos[1]]}
+        cursor.style.width = '200px'
+        cursor.style.borderRadius = '10px'
+        cursor.style.pointerEvents = 'unset'
+        cursor.style.backgroundColor = 'white'
+        cursor.style.outline = '2px solid black'
+    
+    
+    
+        cursor.style.height = `${(buttons.length * 25) + (2 * buttons.length - 1)}px`
+    
+        for(const key in buttons) {
+            const div = document.createElement('div')
+            div.classList.add('contextMenuButton')
+            div.innerText = buttons[key].text
+            div.onclick = () => {
+                buttons[key].action()
+                closeContextMenu()
+            }
+            cursor.append(div)
+        }
+    
+        contextMenuOpen = true
+    }
+}
+
+function closeContextMenu(fix = true) {
+    if(fix) {
+        mouseTarget = () => {return [mouse[0], mouse[1]]}
+        cursor.style.borderRadius = '50%'
+        cursor.style.width = '15px'
+        cursor.style.height = '15px'
+        cursor.style.pointerEvents = 'none'
+        cursor.innerHTML = ''
+        cursor.style.backgroundColor = 'transparent'
+        cursor.style.outline = 'none'
+        cursor.style.cursor = 'none'
+        cursor.style.outline = 'none'
+        cursor.style.backdropFilter = 'invert()'
+        cursorText.innerText = ''
+        cursor.innerText = ''
+        cursor.style.color = 'white'
+    }
+    contextMenuOpen = false
+}
---
+++ script.js
@@ -0,0 +1,308 @@
+const art = {
+    goober: [
+        {
+            url: 'checkThisOut.png',
+            date: 'April 28th, 2024'
+        },
+        {
+            url: 'Nyan.png',
+            date: 'May 16th, 2024'
+        }
+    ],
+    notebook: [
+        {
+            url: 'Bear.jpg',
+            date: 'March 1st, 2024'
+        },
+        {
+            url: 'cubanPerson.jpg',
+            date: 'March 1st, 2024'
+        },
+        {
+            url: 'Hank0.jpg',
+            date: 'March 1st, 2024'
+        },
+        {
+            url: 'Hank1.png',
+            date: 'July 21st, 2023'
+        },
+    ],
+    pixel: [
+        {
+            url: 'desert.png',
+            date: 'March 16th, 2024'
+        },
+        {
+            url: 'donut.png',
+            date: 'March 16th, 2024'
+        },
+        {
+            url: 'guy.png',
+            date: 'March 15th, 2024'
+        },
+        {
+            url: 'helloBro.png',
+            date: 'March 16th, 2024'
+        },
+        {
+            url: 'Tree.png',
+            date: 'March 15th, 2024'
+        }
+    ],
+    vector: [
+        {
+            url: 'BlackHole.png',
+            date: 'August 19th, 2023'
+        },
+        {
+            url: 'butter.png',
+            date: 'December 20th, 2022'
+        },
+        {
+            url: 'GDIcon.png',
+            date: 'December 1st, 2022'
+        },
+        {
+            url: 'ruinseekerballing.png',
+            date: 'March 17th, 2024'
+        },
+        {
+            url: 'Tower.png',
+            date: 'February 7th, 2023'
+        },
+        {
+            url: 'Tower2.png',
+            date: 'March 3rd, 2023'
+        },
+        {
+            url: 'Tower3.png',
+            date: 'August 10th, 2023'
+        },
+        {
+            url: 'Triangles.png',
+            date: 'August 19th, 2023'
+        }
+    ],
+}
+
+const artTitles = {
+    goober: 'Goober Art',
+    notebook: 'Notebook Doodles',
+    pixel: 'Pixel Art',
+    vector: 'Vector Art',
+}
+
+const textures = [
+    'guy.png',
+]
+
+for(const group in art) {
+    for(const peice in art[group]) {
+        if(!art[group][peice].folder) {
+            textures.push(`art/${group}/${art[group][peice].url}`)
+        }
+    }
+}
+
+
+const gallery = doge('gallery')
+const innerGallery = doge('innerGallery')
+const galleryContainer = doge('galleryContainer')
+function openGallery(group) {
+    console.log(group)
+    innerGallery.innerHTML = ''
+    closeMediaView()
+    gallery.style.animation = 'galleryOpen 250ms cubic-bezier(0,1,.5,1) 1 forwards'
+    setTimeout(() => {
+        galleryContainer.style.pointerEvents = 'unset'
+    }, 250);
+
+    doge('galleryHeaderText').innerText = '// ' + artTitles[group]
+    doge('galleryFooterText').innerText = art[group].length + ' Items'
+
+    //Clear images
+    
+    innerGallery.innerHTML = ''
+
+    //Add images
+
+    for(const image in art[group]) {
+        if(!art[group][image].folder) {
+            const img = document.createElement('img')
+            img.src = `media/art/${group}/${art[group][image].url}`
+            innerGallery.append(img)
+    
+            img.onmouseenter = () => {
+                if(!contextMenuOpen) {
+                    const rect = img.getBoundingClientRect()
+                    mouseTarget = () => {return [rect.left + img.offsetWidth / 2, rect.top + img.offsetHeight / 2]}
+                    cursor.style.borderRadius = '0'
+                    cursor.style.width = img.offsetWidth + 'px'
+                    cursor.style.height = img.offsetHeight + 'px'
+                    cursor.style.outline = '2px solid white'
+                    cursor.style.backdropFilter = 'none'
+                    cursorHovering = img
+                    cursorText.innerText = art[group][image].url
+                }
+            }
+            img.onmouseleave = () => {
+                if(!contextMenuOpen) {
+                    mouseTarget = () => {return [mouse[0], mouse[1]]}
+                    cursor.style.borderRadius = '50%'
+                    cursor.style.width = '15px'
+                    cursor.style.height = '15px'
+                    cursor.style.outline = 'none'
+                    cursor.style.backdropFilter = 'invert()'
+                    cursorHovering = undefined
+                    cursorText.innerText = ''
+                }
+            }
+            img.onclick = () => {
+                openMediaView(group, art[group][image])
+            }
+        } else {
+            const folder = document.createElement('button')
+            folder.style.height = '200px'
+            folder.style.width = '200px'
+            folder.style.fontSize = '1.5em'
+            folder.style.fontWeight = '700'
+            folder.innerText = art[group][image].name
+
+            folder.onmouseenter = () => {
+                if(!contextMenuOpen) {
+                    const rect = folder.getBoundingClientRect()
+                    mouseTarget = () => {return [rect.left + folder.offsetWidth / 2, rect.top + folder.offsetHeight / 2]}
+                    cursor.style.borderRadius = '0'
+                    cursor.style.width = folder.offsetWidth + 'px'
+                    cursor.style.height = folder.offsetHeight + 'px'
+                    cursorHovering = folder
+                }
+            }
+            folder.onmouseleave = () => {
+                if(!contextMenuOpen) {
+                    mouseTarget = () => {return [mouse[0], mouse[1]]}
+                    cursor.style.borderRadius = '50%'
+                    cursor.style.width = '15px'
+                    cursor.style.height = '15px'
+                    cursorHovering = undefined
+                }
+            }
+
+            folder.onclick = () => {
+                openGallery(art[group][image].open)
+            }
+
+            innerGallery.append(folder)
+        }
+    }
+}
+
+function closeGallery() {
+    gallery.style.animation = 'galleryClose 250ms ease-in 1 forwards'
+    galleryContainer.style.pointerEvents = 'none'
+}
+
+const mediaView = doge('mediaView')
+function openMediaView(group, img) {
+    mediaView.style.animation = 'galleryOpen 250ms cubic-bezier(0,1,.5,1) 1 forwards'
+    doge('mediaViewImg').src = `media/art/${group}/${img.url}`
+    doge('mediaViewTitle').innerText = img.url
+    doge('mediaViewDate').innerText = img.date
+    
+    setTimeout(() => {
+        mediaView.style.pointerEvents = 'unset'
+    }, 250);
+}
+
+function closeMediaView() {
+    mediaView.style.animation = 'galleryClose 250ms ease-in 1 forwards'
+    mediaView.style.pointerEvents = 'none'
+
+}
+
+
+let allTexturesLoaded = false
+
+function loadTexures(path) {
+    let loaded = 0
+    let total = textures.length
+    for(const image in path) {
+        const img = new Image()
+        img.onload = () => {
+            loaded++
+
+            doge('texturesLoaded').innerText = `${loaded}/${total}`
+            doge('loadingBar').style.width = (loaded / total) * 100 + '%'
+
+            if(loaded === total) {
+                allTexturesLoaded = true
+                if(allTexturesLoaded && allVideosLoaded) {
+                    doge('texturesLoaded').innerText = 'All media loaded!'
+                    doge('loadingScreen').style.opacity = 0
+                    document.body.style.overflow = 'unset'
+                    setTimeout(() => {
+                        doge('loadingScreen').style.display = 'none'
+                    }, 500);
+                }
+            }
+        }
+        img.src = `media/${path[image]}`
+    }
+}
+loadTexures(textures)
+
+const videos = [
+    'headerBG'
+]
+let allVideosLoaded = false
+
+function loadVideos(path) {
+    let loadCheckInterval = setInterval(() => {
+        let loaded = true
+        for(const video in path) {
+            if(doge(path[video]).readyState !== 4) {
+                loaded = false
+            }
+        }
+
+        if(loaded) {
+            clearInterval(loadCheckInterval)
+            allVideosLoaded = true
+
+            if(allTexturesLoaded && allVideosLoaded) {
+                doge('texturesLoaded').innerText = 'All media loaded!'
+                doge('loadingScreen').style.opacity = 0
+                document.body.style.overflow = 'unset'
+                setTimeout(() => {
+                    doge('loadingScreen').style.display = 'none'
+                }, 500);
+            }
+        }
+    }, 500);
+}
+
+loadVideos(videos)
+
+
+
+function guyClick() {
+    doge('guyCounter').innerText++
+    doge('guyCounter').style.opacity = 1
+
+    doge('guyCounter').style.animation = 'none'
+    requestAnimationFrame(() => {
+        doge('guyCounter').style.animation = 'pulse 500ms cubic-bezier(0,1,.5,1) 1 forwards'
+    })
+}
+
+doge('guy').onmouseenter = () => {
+    cursor.style.width = 0
+    cursor.style.height = 0
+    realCursor.style.opacity = 0
+}
+
+doge('guy').onmouseleave = () => {
+    cursor.style.width = '15px'
+    cursor.style.height = '15px'
+    realCursor.style.opacity = 1
+}
